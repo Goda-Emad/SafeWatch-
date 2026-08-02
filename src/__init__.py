fb2bc41a0f43b0d4833a59bb977547706cc7aeaf
goda emad <goda.emade2001@gmail.com>
2026-08-02 16:20:23 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,6 @@
+# SafeWatch - src/__init__.py
 
+from .predict import predict_image
+from .preprocess import preprocess_image
+from .alert import check_alert
+from .email_sender import send_alert_email
